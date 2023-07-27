4af829a4182109a91cc14750bbd4e9231c96b936
Leehongseok-code <52267586+Leehongseok-code@users.noreply.github.com>
2023-07-28 00:18:19 +0900

    models.py 최신화

--- blockchain/models.py
+++ blockchain/models.py
@@ -1,3 +1,10 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
 
@@ -68,6 +75,18 @@ class AuthUserUserPermissions(models.Model):
         managed = False
         db_table = 'auth_user_user_permissions'
         unique_together = (('user', 'permission'),)
+
+
+class CoinNews(models.Model):
+    news_id = models.IntegerField(primary_key=True)
+    news_title = models.TextField(blank=True, null=True)
+    thumb_url = models.TextField(blank=True, null=True)
+    view = models.IntegerField(blank=True, null=True)
+    src = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'coin_news'
 
 
 class DjangoAdminLog(models.Model):
