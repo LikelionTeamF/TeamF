f93006707c5073d1f4c1afd95d23e5b6402cd1c4
jeongmyunghee <myunghee1231@naver.com>
2023-08-16 21:46:45 +0900

    jjj

--- subscribe.py
+++ subscribe.py
@@ -1,9 +1,9 @@
 from newsletter import NewsLetter
-
+from blockchain.models import Subscribers
 def SendToSubscribers():
     #모델에서 subscribers끌어와서 리스트로 대치
-    subscribers = []
+    subscribers = Subscribers.objects.all() 
     for subscriber in subscribers:
-        NewsLetter(subscriber)
+        NewsLetter(subscriber.email)
         print(subscriber + " send")
 
